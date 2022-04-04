4913e1aec9f59538f49c21c0eae27d387120820e
Maxim <maksim.mystafa@gmail.com>
2022-04-04 14:41:47 +0300

    add real time changes of noise with trackbar

--- Homework 4/Task.py
+++ Homework 4/Task.py
@@ -46,38 +46,32 @@ dim = (width, height)
 image = cv.resize(source_image, dim, interpolation=cv.INTER_AREA)
 
 prob = 0
+last_prob = 1
 
 cv.namedWindow('source_image')
 cv.createTrackbar('Prob', 'source_image', 0, 100, nothing)
 
 while True:
-    cv.imshow('source_image', image)
-
-    k = cv.waitKey(0)
-
     prob = cv.getTrackbarPos('Prob', 'source_image')
 
-    if k == ord('e'):
+    k = cv.waitKey(1)
+
+    if prob != last_prob:
+
+        cv.imshow('source_image', image)
         noise_image = salt_pepper_noise(image, float(prob) / 100)
         averaging_blur = cv.blur(noise_image, ksize=(15, 15))
         median_blur = cv.medianBlur(noise_image, ksize=15)
         gaussian_blur = cv.GaussianBlur(noise_image, ksize=(15, 15), sigmaX=0, sigmaY=0)
         bilateral_filter = cv.bilateralFilter(src=noise_image, d=9, sigmaColor=75, sigmaSpace=75)
 
-        noise_name = 'noise_image_' + str(prob)
-
-        cv.imshow(noise_name, noise_image)
+        cv.imshow('noise_image', noise_image)
         cv.imshow('averaging_blur', averaging_blur)
         cv.imshow('median_blur', median_blur)
         cv.imshow('gaussian_blur', gaussian_blur)
         cv.imshow('bilateral_filter', bilateral_filter)
 
-    if k == ord('c'):
-        cv.destroyWindow(noise_name)
-        cv.destroyWindow('averaging_blur')
-        cv.destroyWindow('median_blur')
-        cv.destroyWindow('gaussian_blur')
-        cv.destroyWindow('bilateral_filter')
+    last_prob = prob
 
     if k == ord('q'):
         cv.destroyAllWindows()
